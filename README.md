070644e6fd99765794ec1e20d44b0f310eed5a93
Daniel Mantovani <daniel@gmail.com>
2018-12-22 20:43:44 -0300

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojo::Iutils - It's new $module
+
+# SYNOPSIS
+
+    use Mojo::Iutils;
+
+# DESCRIPTION
+
+Mojo::Iutils is ...
+
+# LICENSE
+
+Copyright (C) Daniel Mantovani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Daniel Mantovani <daniel@gmail.com>
---
+++ lib/Mojo/Iutils.pm
@@ -0,0 +1,39 @@
+package Mojo::Iutils;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::Iutils - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojo::Iutils;
+
+=head1 DESCRIPTION
+
+Mojo::Iutils is ...
+
+=head1 LICENSE
+
+Copyright (C) Daniel Mantovani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Daniel Mantovani E<lt>daniel@gmail.comE<gt>
+
+=cut
+
